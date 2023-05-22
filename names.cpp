47a5ce621dca068d9345cb0165544947b7f57ede
Dumitru Postoronca <103861986+DdimaPos@users.noreply.github.com>
2023-05-22 21:27:18 +0300

    Add files via upload

---
+++ names.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main() {
+    std::string folderPath = "img/fotosesie"; // Замените на путь к вашей папке
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        if (entry.is_regular_file()) {
+            std::cout <<"'" <<"../img/fotosesie/"<< entry.path().filename().string() << "'" <<","<< std::endl;
+        }
+    }
+    return 0;
+}
