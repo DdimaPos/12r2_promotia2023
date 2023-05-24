000337d8fe15af5c808fe7de259eb0eac773e9c1
DdimaPos <postoroncadumitru@gmail.com>
2023-05-24 10:22:35 +0300

    photo arrangements

--- names.cpp
+++ names.cpp
@@ -6,7 +6,7 @@ int main() {
     std::string folderPath = "img/fotosesie"; // Замените на путь к вашей папке
     for (const auto& entry : fs::directory_iterator(folderPath)) {
         if (entry.is_regular_file()) {
-            std::cout <<"'" <<"../img/fotosesie/"<< entry.path().filename().string() << "'" <<","<< std::endl;
+            std::cout <<"'" <<"img/fotosesie/"<< entry.path().filename().string() << "'" <<","<< std::endl;
         }
     }
     return 0;
