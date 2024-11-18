e65bb9bfb23b26b02b52f360e091d1ea38849305
DdimaPos <postoroncadumitru@gmail.com>
2024-11-18 23:56:25 +0200

    feat: Lazy loading for photo-session page
    feat: Lazy loading for 30 mai page

--- js/script.js
+++ js/script.js
@@ -1,139 +1,170 @@
 var images = [
-'img/fotosesie/image-113.jpg',
-'img/fotosesie/image-7.jpg',
-'img/fotosesie/image-114.jpg',
-'img/fotosesie/image-124.jpg',
-'img/fotosesie/image-134.jpg',
-'img/fotosesie/image-56.jpg',
-'img/fotosesie/image-138.jpg',
-'img/fotosesie/image-139.jpg',
-'img/fotosesie/image-149.jpg',
-'img/fotosesie/image-151.jpg',
-'img/fotosesie/image-156.jpg',
-'img/fotosesie/image-159.jpg',
-'img/fotosesie/image-164.jpg',
-'img/fotosesie/image-168.jpg',
-'img/fotosesie/image-234.jpg',
-'img/fotosesie/image-238.jpg',
-'img/fotosesie/image-109.jpg',
-'img/fotosesie/image-25.jpg',
-'img/fotosesie/image-251.jpg',
-'img/fotosesie/image-259.jpg',
-'img/fotosesie/image-272.jpg',
-'img/fotosesie/image-274.jpg',
-'img/fotosesie/image-275.jpg',
-'img/fotosesie/image-281.jpg',
-'img/fotosesie/image-285.jpg',
-'img/fotosesie/image-286.jpg',
-'img/fotosesie/image-297.jpg',
-'img/fotosesie/image-300.jpg',
-'img/fotosesie/image-302.jpg',
-'img/fotosesie/image-304.jpg',
-'img/fotosesie/image-309.jpg',
-'img/fotosesie/image-323.jpg',
-'img/fotosesie/image-165.jpg',
-'img/fotosesie/image-178.jpg',
-'img/fotosesie/image-187.jpg',
-'img/fotosesie/image-190.jpg',
-'img/fotosesie/image-217.jpg',
-'img/fotosesie/image-329.jpg',
-'img/fotosesie/image-332.jpg',
-'img/fotosesie/image-350.jpg',
-'img/fotosesie/image-352-101.jpg',
-'img/fotosesie/image-352-108.jpg',
-'img/fotosesie/image-352-110.jpg',
-'img/fotosesie/image-352-111.jpg',
-'img/fotosesie/image-352-114.jpg',
-'img/fotosesie/image-352-120.jpg',
-'img/fotosesie/image-352-128.jpg',
-'img/fotosesie/image-352-14.jpg',
-'img/fotosesie/image-352-146.jpg',
-'img/fotosesie/image-352-147.jpg',
-'img/fotosesie/image-352-149.jpg',
-'img/fotosesie/image-352-150.jpg',
-'img/fotosesie/image-352-151.jpg',
-'img/fotosesie/image-352-153.jpg',
-'img/fotosesie/image-352-155.jpg',
-'img/fotosesie/image-352-156.jpg',
-'img/fotosesie/image-352-161.jpg',
-'img/fotosesie/IMG_1119.JPG',
-'img/fotosesie/image-352-163.jpg',
-'img/fotosesie/image-352-169.jpg',
-'img/fotosesie/image-352-170.jpg',
-'img/fotosesie/image-352-174.jpg',
-'img/fotosesie/image-352-177.jpg',
-'img/fotosesie/image-352-179.jpg',
-'img/fotosesie/image-352-183.jpg',
-'img/fotosesie/image-352-191.jpg',
-'img/fotosesie/image-352-203.jpg',
-'img/fotosesie/image-352-207.jpg',
-'img/fotosesie/image-352-209.jpg',
-'img/fotosesie/image-352-210.jpg',
-'img/fotosesie/image-352-214.jpg',
-'img/fotosesie/image-352-216.jpg',
-'img/fotosesie/image-352-217.jpg',
-'img/fotosesie/image-352-219.jpg',
-'img/fotosesie/image-352-227.jpg',
-'img/fotosesie/image-352-230.jpg',
-'img/fotosesie/image-352-233.jpg',
-'img/fotosesie/image-352-243.jpg',
-'img/fotosesie/image-352-246.jpg',
-'img/fotosesie/image-352-252.jpg',
-'img/fotosesie/image-352-259.jpg',
-'img/fotosesie/image-352-261.jpg',
-'img/fotosesie/image-352-266.jpg',
-'img/fotosesie/image-352-268.jpg',
-'img/fotosesie/image-352-270.jpg',
-'img/fotosesie/image-352-273.jpg',
-'img/fotosesie/image-352-276.jpg',
-'img/fotosesie/image-352-279.jpg',
-'img/fotosesie/image-352-28.jpg',
-'img/fotosesie/image-352-281.jpg',
-'img/fotosesie/image-352-292.jpg',
-'img/fotosesie/image-352-3.jpg',
-'img/fotosesie/image-352-4.jpg',
-'img/fotosesie/image-352-42.jpg',
-'img/fotosesie/image-352-46.jpg',
-'img/fotosesie/image-352-50.jpg',
-'img/fotosesie/image-352-51.jpg',
-'img/fotosesie/image-352-52.jpg',
-'img/fotosesie/image-352-6.jpg',
-'img/fotosesie/image-352-63.jpg',
-'img/fotosesie/image-352-71.jpg',
-'img/fotosesie/image-352-84.jpg',
-'img/fotosesie/image-352-87.jpg',
-'img/fotosesie/image-352-89.jpg',
-'img/fotosesie/image-352-97.jpg',
-'img/fotosesie/image-352.jpg',
-'img/fotosesie/image-58.jpg',
-'img/fotosesie/image-80.jpg',
-'img/fotosesie/image-89.jpg',
-'img/fotosesie/image-96.jpg',
-  ]; 
+  "img/fotosesie/image-113.jpg",
+  "img/fotosesie/image-7.jpg",
+  "img/fotosesie/image-114.jpg",
+  "img/fotosesie/image-124.jpg",
+  "img/fotosesie/image-134.jpg",
+  "img/fotosesie/image-56.jpg",
+  "img/fotosesie/image-138.jpg",
+  "img/fotosesie/image-139.jpg",
+  "img/fotosesie/image-149.jpg",
+  "img/fotosesie/image-151.jpg",
+  "img/fotosesie/image-156.jpg",
+  "img/fotosesie/image-159.jpg",
+  "img/fotosesie/image-164.jpg",
+  "img/fotosesie/image-168.jpg",
+  "img/fotosesie/image-234.jpg",
+  "img/fotosesie/image-238.jpg",
+  "img/fotosesie/image-109.jpg",
+  "img/fotosesie/image-25.jpg",
+  "img/fotosesie/image-251.jpg",
+  "img/fotosesie/image-259.jpg",
+  "img/fotosesie/image-272.jpg",
+  "img/fotosesie/image-274.jpg",
+  "img/fotosesie/image-275.jpg",
+  "img/fotosesie/image-281.jpg",
+  "img/fotosesie/image-285.jpg",
+  "img/fotosesie/image-286.jpg",
+  "img/fotosesie/image-297.jpg",
+  "img/fotosesie/image-300.jpg",
+  "img/fotosesie/image-302.jpg",
+  "img/fotosesie/image-304.jpg",
+  "img/fotosesie/image-309.jpg",
+  "img/fotosesie/image-323.jpg",
+  "img/fotosesie/image-165.jpg",
+  "img/fotosesie/image-178.jpg",
+  "img/fotosesie/image-187.jpg",
+  "img/fotosesie/image-190.jpg",
+  "img/fotosesie/image-217.jpg",
+  "img/fotosesie/image-329.jpg",
+  "img/fotosesie/image-332.jpg",
+  "img/fotosesie/image-350.jpg",
+  "img/fotosesie/image-352-101.jpg",
+  "img/fotosesie/image-352-108.jpg",
+  "img/fotosesie/image-352-110.jpg",
+  "img/fotosesie/image-352-111.jpg",
+  "img/fotosesie/image-352-114.jpg",
+  "img/fotosesie/image-352-120.jpg",
+  "img/fotosesie/image-352-128.jpg",
+  "img/fotosesie/image-352-14.jpg",
+  "img/fotosesie/image-352-146.jpg",
+  "img/fotosesie/image-352-147.jpg",
+  "img/fotosesie/image-352-149.jpg",
+  "img/fotosesie/image-352-150.jpg",
+  "img/fotosesie/image-352-151.jpg",
+  "img/fotosesie/image-352-153.jpg",
+  "img/fotosesie/image-352-155.jpg",
+  "img/fotosesie/image-352-156.jpg",
+  "img/fotosesie/image-352-161.jpg",
+  "img/fotosesie/IMG_1119.JPG",
+  "img/fotosesie/image-352-163.jpg",
+  "img/fotosesie/image-352-169.jpg",
+  "img/fotosesie/image-352-170.jpg",
+  "img/fotosesie/image-352-174.jpg",
+  "img/fotosesie/image-352-177.jpg",
+  "img/fotosesie/image-352-179.jpg",
+  "img/fotosesie/image-352-183.jpg",
+  "img/fotosesie/image-352-191.jpg",
+  "img/fotosesie/image-352-203.jpg",
+  "img/fotosesie/image-352-207.jpg",
+  "img/fotosesie/image-352-209.jpg",
+  "img/fotosesie/image-352-210.jpg",
+  "img/fotosesie/image-352-214.jpg",
+  "img/fotosesie/image-352-216.jpg",
+  "img/fotosesie/image-352-217.jpg",
+  "img/fotosesie/image-352-219.jpg",
+  "img/fotosesie/image-352-227.jpg",
+  "img/fotosesie/image-352-230.jpg",
+  "img/fotosesie/image-352-233.jpg",
+  "img/fotosesie/image-352-243.jpg",
+  "img/fotosesie/image-352-246.jpg",
+  "img/fotosesie/image-352-252.jpg",
+  "img/fotosesie/image-352-259.jpg",
+  "img/fotosesie/image-352-261.jpg",
+  "img/fotosesie/image-352-266.jpg",
+  "img/fotosesie/image-352-268.jpg",
+  "img/fotosesie/image-352-270.jpg",
+  "img/fotosesie/image-352-273.jpg",
+  "img/fotosesie/image-352-276.jpg",
+  "img/fotosesie/image-352-279.jpg",
+  "img/fotosesie/image-352-28.jpg",
+  "img/fotosesie/image-352-281.jpg",
+  "img/fotosesie/image-352-292.jpg",
+  "img/fotosesie/image-352-3.jpg",
+  "img/fotosesie/image-352-4.jpg",
+  "img/fotosesie/image-352-42.jpg",
+  "img/fotosesie/image-352-46.jpg",
+  "img/fotosesie/image-352-50.jpg",
+  "img/fotosesie/image-352-51.jpg",
+  "img/fotosesie/image-352-52.jpg",
+  "img/fotosesie/image-352-6.jpg",
+  "img/fotosesie/image-352-63.jpg",
+  "img/fotosesie/image-352-71.jpg",
+  "img/fotosesie/image-352-84.jpg",
+  "img/fotosesie/image-352-87.jpg",
+  "img/fotosesie/image-352-89.jpg",
+  "img/fotosesie/image-352-97.jpg",
+  "img/fotosesie/image-352.jpg",
+  "img/fotosesie/image-58.jpg",
+  "img/fotosesie/image-80.jpg",
+  "img/fotosesie/image-89.jpg",
+  "img/fotosesie/image-96.jpg",
+];
 
-var imageGallery = document.querySelector('.gallery');
-
+//populate the dom with the images
+var imageGallery = document.querySelector(".gallery");
+//<img class="anim__item" src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg" data-src="actual.img" alt="Image">
 function displayImages() {
   for (var i = 0; i < images.length; i++) {
-    var image = document.createElement('img');
+    var image = document.createElement("img");
     image.classList.add("anim__item");
-    image.src = images[i]; 
-    image.alt = 'Изображение';
+    image.classList.add("lazy");
+    image.src =
+      "https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg";
+    image.setAttribute("data-src", images[i]);
+    image.alt = "Image";
     imageGallery.appendChild(image);
   }
 }
 
 displayImages();
+
+//implement lazy loading
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function (
+      entries,
+      _,
+    ) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
+});
+
+
+///Menu
 const button = document.querySelector(".menu__icon");
 const menu = document.querySelector(".menu__body");
-button.addEventListener("click", function(ev){
+button.addEventListener("click", function (ev) {
   document.body.classList.toggle("lock");
   button.classList.toggle("active");
   menu.classList.toggle("active");
 });
 
+//Animation
 const animItems = document.querySelectorAll(".anim__item");
-if(animItems.length > 0){
+if (animItems.length > 0) {
   window.addEventListener("scroll", animOnScroll);
   function animOnScroll(params) {
     for (let index = 0; index < animItems.length; index++) {
@@ -141,32 +172,38 @@ if(animItems.length > 0){
       const animItemHeight = animItem.offsetHeight;
       const animItemOffset = offset(animItem).top;
       const animStart = 5;
-      let animItemPoint = window.innerHeight - animItemHeight/animStart;
-      if(animItemHeight > window.innerHeight){
-        animItemPoint = window.innerHeight - window.innerHeight/animStart;
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
       }
 
-      if(index%2){
-        if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset+animItemHeight)){
+      if (index % 2) {
+        if (
+          pageYOffset > animItemOffset - animItemPoint &&
+          pageYOffset < animItemOffset + animItemHeight
+        ) {
           animItem.classList.add("active");
-        }else{
+        } else {
           animItem.classList.remove("active");
         }
-      }else{
+      } else {
         setTimeout(() => {
-          if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset+animItemHeight)){
+          if (
+            pageYOffset > animItemOffset - animItemPoint &&
+            pageYOffset < animItemOffset + animItemHeight
+          ) {
             animItem.classList.add("active");
-          }else{
+          } else {
             animItem.classList.remove("active");
           }
         }, 300);
       }
     }
-    function offset(el){
+    function offset(el) {
       const rect = el.getBoundingClientRect(),
-      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-      scrollTop = window.pageYOffset ||document.documentElement.scrollTop;
-      return{top:rect.top+scrollTop, left: rect.left + scrollLeft}
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
     }
   }
   animOnScroll();
